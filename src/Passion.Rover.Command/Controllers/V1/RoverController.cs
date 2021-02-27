4366a58d26dbfa48834889bce5d9f274a7bdcd2d
odagdelen <odagdelen@THB-DEV-427>
2021-02-27 21:53:00 +0300

    initial commit

---
+++ src/Passion.Rover.Command/Controllers/V1/RoverController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Passion.Rover.Command.Controllers.V1
+{
+    [Route("api/v1/rover")]
+    [ApiController]
+    public class RoverController : ControllerBase
+    {
+        public RoverController()
+        {
+                
+        }
+        
+        [HttpGet("healthcheck")]
+        [ProducesResponseType(typeof(string), (int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        // [Authorize(nameof(GetClaimByClaimNumber))]
+        public HttpStatusCode GetByTest()
+        {
+            return  HttpStatusCode.OK;
+        }
+    }
+}
